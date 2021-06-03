0f4c9c548e18fb9660fbc754c3cf385044936c5d
= <=>
2021-06-03 06:54:55 +0300

    Exercise 6

--- Exercise6.py
+++ Exercise6.py
@@ -18,3 +18,22 @@ def iter_func(iterable):
 
 list1 = [-1,2,5,-3,-8,10,-5,0]
 iter_func(list1)
+
+##########################################################################################
+
+import itertools as it
+def drop_while(nums):
+    return it.dropwhile(lambda x : x < 0, nums)
+nums = [-1,-2,-3,4,-10,2,0,5,12]
+print("Original list: ",nums)
+result = drop_while(nums)
+print("Drops elements from the iterable when a positive number arises \n",list(result))
+#Alternate solution
+def negative_num(x):
+    return x < 0
+def drop_while(nums):
+    return it.dropwhile(negative_num, nums)
+nums = [-1,-2,-3,4,-10,2,0,5,12]
+print("Original list: ",nums)
+result = drop_while(nums)
+print("Drops elements from the iterable when a positive number arises \n",list(result))
